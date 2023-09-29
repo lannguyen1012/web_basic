ac2a4cc63b3e685d28400417007ac4356bba7f8f
= <=>
2023-09-29 20:37:44 +0700

    lesson1

---
+++ JSA/Lesson 1/lesson1.js
@@ -0,0 +1,24 @@
+// Các loại vòng lặp trong Javascript
+// Vòng lặp while
+console.log('vòng lặp while')
+
+let i = 0
+while (i<5){
+    console.log("Giá trị i =", i)
+    i++;
+}
+
+// Vòng lặp dowhile
+console.log("vòng lặp dowhile")
+i = 5
+do {
+    console.log("Giá trị i =", i);
+    i++;
+} while (i < 5);
+
+// Vòng lặp for
+console.log("vòng lặp for")
+for(let j = 0; j < 6; j++){
+    console.log("Giá trị của j =", j);
+
+}
